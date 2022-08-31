1e28884723c24409ff9a4f31efb17e2c3fa02b7b
Dylan Shaffer <SHAFFERDR@ETSU.EDU>
2022-08-31 11:19:55 -0400

    Fixed code issues as per Roach

--- Models/Toy.cs
+++ Models/Toy.cs
@@ -5,6 +5,6 @@ namespace DSLabWeek01.Models;
 public class Toy
 {
     public int Id { get; set; }
-    public string Brand { get; set; }
-    public string Description { get; set; }
+    public string Brand { get; set; } = String.Empty;
+    public string Description { get; set; } = String.Empty;
 }
